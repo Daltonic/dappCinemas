b4d196877545a959ec920c2f016fdb679ed97b19
Kelechi Otugbuali <hilakechi@gmail.com>
2023-02-10 14:10:13 +0100

    feat MovieDetails buy ticket button completed

--- src/pages/MovieDetailsPage.js
+++ src/pages/MovieDetailsPage.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import Emancipation from '../asset/emancipation.jpg'
+import CalendarMonthIcon from '@mui/icons-material/CalendarMonth';
 
 function MovieDetailsPage() {
   return (
@@ -7,11 +8,75 @@ function MovieDetailsPage() {
             <div className='flex w-full '>
                 <img src={Emancipation} className='h-80 w-full object-cover'/>
             </div>
+            <div className='flex flex-col space-y-4 align-center text-center w-full'>
             <div className='flex flex-col space-y-2'>
                 <h3 className='font-black text-2xl'>Emmancipation</h3>
                 <p className='text-gray-700'>Crime,Drama,Thrill</p>
                 <p className='text-gray-700'>Runtime: 2h 2min</p>
             </div>
+            <div className='flex text-center align-center m-auto space-x-8'>
+                <p className='font-bold'>DATE: <span className='font-thin'>February 9, Thursday</span></p>
+                <CalendarMonthIcon className='text-gray-600'/>
+
+            </div>
+            <div className='grid grid-cols-1  gap-4 p-2'>
+           
+            <div className='flex align-center items-center justify-center  space-x-4  bg-gray-300 rounded-md p-2  m-auto w-full md: 4/5 md:w-2/3'>
+            <p className=''>11:30 AM</p>
+            <sub>5/10 <span>spaces</span></sub>
+            <button className="bg-black py-1 px-4 text-xs font-bold text-white border-2 border-black
+            hover:bg-transparent  rounded-full
+             hover:border-2 hover:border-red-600 hover:text-black ">
+              BUY TICKET
+            </button>
+          </div>
+
+          
+
+          <div className='flex align-center items-center justify-center  space-x-4  bg-gray-300 rounded-md p-2  m-auto w-full md: 4/5 md:w-2/3'>
+          <p>2:30 PM</p>
+          <sub>5/10 <span>spaces</span></sub>
+            <button className="bg-black py-1 px-4 text-xs font-bold text-white border-2 border-black
+            hover:bg-transparent  rounded-full
+             hover:border-2 hover:border-red-600 hover:text-black ">
+              BUY TICKET
+            </button>
+          </div>
+       
+
+        
+
+          <div className='flex align-center items-center justify-center  space-x-4  bg-gray-300 rounded-md p-2  m-auto w-full md: 4/5 md:w-2/3'>
+          <p>4:15 PM</p>
+          <sub>5/10 <span>spaces</span></sub>
+            <button className="bg-black py-1 px-4 text-xs font-bold text-white border-2 border-black
+            hover:bg-transparent  rounded-full
+             hover:border-2 hover:border-red-600 hover:text-black ">
+              BUY TICKET
+            </button>
+          </div>
+
+          <div className='flex align-center items-center justify-center  space-x-4  bg-gray-300 rounded-md p-2  m-auto w-full md: 4/5 md:w-2/3'>
+          <p>8:30 PM</p>
+          <sub>5/10 <span>spaces</span></sub>
+            <button className="bg-black py-1 px-4 text-xs font-bold text-white border-2 border-black
+            hover:bg-transparent  rounded-full
+             hover:border-2 hover:border-red-600 hover:text-black ">
+              BUY TICKET
+            </button>
+          </div>
+
+         
+
+         
+                
+               
+              
+            </div>
+        
+            </div>
+           
+         
     </div>
   )
 } 
